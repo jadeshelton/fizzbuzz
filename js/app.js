21a86a6739124457da4e8016fb49af8a4a5fc5b2
jadeshelton <jnshelton257@yahoo.com>
2015-11-09 21:20:12 -0800

    commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+for (i = 1; i < 101; i++) { 
+if (i % 3 == 0 && i % 5 == 0) { 
+	document.write("FizzBuzz" + "<br>" + "<br>"); 
+} 
+else if (i % 3 == 0) { 
+	document.write("Fizz" + "<br>" + "<br>"); 
+} 
+else if (i % 5 == 0) { 
+	document.write("Buzz" + "<br>" + "<br>"); 
+} 
+else { 
+	document.write(i + "<br>" + "<br>"); 
+} 
+}
+});
